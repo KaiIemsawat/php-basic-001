9acc4db1d29a74989ecbaecc6ff11f9e7f118256
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-01-19 13:44:03 -0500

    php array function part 2

--- 07_array_functions.php
+++ 07_array_functions.php
@@ -17,7 +17,43 @@
     unset($fruits[2]); // remove item at specific inddex
     print_r($fruits);
     
-    $chunked_array = array_chunk($fruits, 2); // Split an array to smaller arrays
-    print_r($chunked_array);
+    // $chunked_array = array_chunk($fruits, 2); // Split an array to smaller arrays
+    // print_r($chunked_array);
+
+
+    $arr1 = [1,2,3];
+    $arr2 = [4,5,6];
+
+    $arr3 = array_merge($arr1, $arr2);
+    $arr4 = [...$arr1, ...$arr2];
+
+    print_r($arr3);
+    print_r($arr4);
+
+    $a = ["green", "red", "yellow"];
+    $b = ["kiwi", "apple", "pineapple"];
+
+    $c = array_combine($a, $b); // $a will be key and $b will be value. $a and $b need to have the same length
+    print_r($c);
     
+    $flipped = array_flip($c); // switch keys and values of an array
+    print_r($flipped);
+
+    $keys = array_keys($c);
+    print_r($keys);
+
+    $numbers = range(10, 20);
+    print_r($numbers);
+
+    $newNumbers = array_map(function($number) {
+        return "Number $number";
+    }, $numbers);
+
+    print_r($newNumbers);
+
+    $lessThan15 = array_filter($numbers, fn($number) => $number < 15);
+    print_r($lessThan15);
+
+    $sum = array_reduce($numbers, fn($carry, $number) => $carry + $number); // to sum all numbers in an array
+    var_dump($sum);
 ?>
